87f98f77017e6315924a999221bccc19d7e5cde8
Roman Kolivashko <rkolivashko@gmail.com>
2021-09-07 14:02:30 -0700

    Gif search is running.
    Co-authored-by: Anthony McRae <anthonytoddmcrae@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,26 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+  $('#giphySearch').click(function() {
+    const gif = $('#giphy').val();
+    $('#giphy').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${gif}&api_key=${process.env.API_KEY}&limit=5`;
+
+//  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    console.log(url);
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
-    }
+    };
 
     request.open("GET", url, true);
     request.send();
-
+    
+    console.log(url);
     function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showGif').html(`<img src="${response.data[0].images.original.url}">`);
+      console.log(response.data[0].images.original.url);
     }
+
   });
 });
